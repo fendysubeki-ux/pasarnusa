7b308efb8b5a81439b4796a816c1355f06ac0353
fendysubeki-ux <fendysubeki@gmail.com>
2026-06-10 19:50:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -163,21 +163,3 @@ previewImage.style.display =
 document.getElementById("simpanProduk").addEventListener("click", function() {
 
 });
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
-  getFirestore,
-  collection,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDq9vebvgycrR27JMQ4Mlnf5JsgZu5KeQk",
-  authDomain: "pasarnusa-18aa0.firebaseapp.com",
-  projectId: "pasarnusa-18aa0",
-  storageBucket: "pasarnusa-18aa0.firebasestorage.app",
-  messagingSenderId: "866998011671",
-  appId: "1:866998011671:web:5555115feb82741ab55952"
-};
-
